13910602ea5a53ce93f1b64f51bb05ae708eb9f8
pavelzelenevichY <pzelenevich@codifi.me>
2021-03-17 18:07:29 +0300

    [MMT-5] Create custom attribute credit hold

---
+++ Training/Setup/Patch/Data/AddCustomerAttributeCreditHold.php
@@ -0,0 +1,110 @@
+<?php
+
+
+namespace Codifi\Training\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Customer\Setup\CustomerSetup;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
+
+class AddCustomerAttributeCreditHold implements DataPatchInterface, PatchRevertableInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * @var CustomerSetup
+     */
+    private $customerSetupFactory;
+    /**
+     * @var AttributeSetFactory
+     */
+    protected $attributeSetFactory;
+
+    /**
+     * Constructor
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param CustomerSetupFactory $customerSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        CustomerSetupFactory $customerSetupFactory,
+        AttributeSetFactory $attributeSetFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->attributeSetFactory = $attributeSetFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var CustomerSetup $customerSetup */
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $customerSetup->addAttribute(
+            \Magento\Customer\Model\Customer::ENTITY,
+            'credit_hold',
+            [
+                'type' => 'int',
+                'label' => 'Credit Hold',
+                'input' => 'select',
+                'source' => \Magento\Eav\Model\Entity\Attribute\Source\Table::class,
+                'option' => ['values' => ['Yes', 'No']],
+                'sort_order' => '30',
+                'visible' => true,
+                'required' => false,
+                'user_defined' => false,
+                'default' => '0',
+                'unique' => false,
+                'system' => false,
+                'is_visible_in_grid' => true,
+                'is_used_in_grid' => true,
+                'is_filterable_in_grid' => true,
+                'is_searchable_in_grid' => true,
+            ]
+        );
+
+        $attribute = $customerSetup->getEavConfig()->getAttribute('customer', 'credit_hold')->addData([
+            'used_in_forms' => [
+                'adminhtml_customer'
+            ]
+        ]);
+        $attribute->save();
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var CustomerSetup $customerSetup */
+        $customerSetup = $this->attributeSetFactory->create(['setup' => $this->moduleDataSetup]);
+        $customerSetup->removeAttribute(\Magento\Customer\Model\Customer::ENTITY, 'credit_hold');
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+}
