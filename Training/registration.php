cafbd3a70ebfa79b4acef5f8fe5b6e5a01ba2f1c
pavelzelenevichY <pzelenevich@codifi.me>
2021-03-17 11:49:06 +0300

    Correction

--- Training/registration.php
+++ Training/registration.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Codifi_Training
+ *
+ * @copyright   Copyright (c) 2021 Codifi
+ * @author      Pavel Zelenevich <pzelenevich@codifi.me>
+ */
 
 use Magento\Framework\Component\ComponentRegistrar;
 
