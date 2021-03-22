b3f4a2f264486e4ee18373145c6cf65f0783c54f
pavelzelenevichY <pzelenevich@codifi.me>
2021-03-22 18:13:18 +0300

    [MMT-8] - Add once show message to admin.

--- Training/Block/Adminhtml/Edit/Tab/View/PersonalInfo.php
+++ Training/Block/Adminhtml/Edit/Tab/View/PersonalInfo.php
@@ -9,6 +9,7 @@
 namespace Codifi\Training\Block\Adminhtml\Edit\Tab\View;
 
 use Codifi\Training\Block\Account\Dashboard\CreditHold;
+use Codifi\Training\Setup\Patch\Data\AddCustomerAttributeCreditHold;
 use Magento\Backend\Block\Template;
 use Magento\Backend\Block\Template\Context;
 use Magento\Backend\Model\Auth\Session;
@@ -20,40 +21,151 @@ use Magento\Backend\Model\Auth\Session;
 class PersonalInfo extends Template
 {
     /**
+     * Customer
+     *
+     * @var \Magento\Customer\Api\Data\CustomerInterface
+     */
+    protected $customer;
+
+    /**
+     * Customer data factory
+     *
+     * @var \Magento\Customer\Api\Data\CustomerInterfaceFactory
+     */
+    protected $customerDataFactory;
+
+    /**
+     * Credit hold
+     *
      * @var CreditHold
      */
     public $credithold;
 
     /**
+     * Admin session.
+     *
      * @var Session
      */
     protected $authSession;
 
     /**
-     * @var \Magento\Customer\Block\Adminhtml\Edit\Tab\View\PersonalInfo
+     * Customers id.
      */
-    public $personalInfoMage;
+    public $customersId;
 
+    /**
+     * PersonalInfo constructor.
+     * @param Context $context
+     * @param CreditHold $creditHold
+     * @param Session $authSession
+     * @param array $data
+     */
     public function __construct(
         Context $context,
         CreditHold $creditHold,
+        Session $authSession,
         array $data = []
     ) {
         $this->credithold = $creditHold;
+        $this->authSession = $authSession;
         parent::__construct($context, $data);
     }
 
-    public function getCustomer()
+    /**
+     * Get options enabled.
+     *
+     * @return bool
+     */
+    public function isCreditHoldConfigEnabled() : bool
     {
-        return $this->personalInfoMage->getCustomer();
+        return $this->credithold->isCreditHoldConfigEnabled();
     }
 
-    public function getOptions()
+    /**
+     * Get credit_hold attribute value
+     *
+     * @return bool
+     */
+    public function getCustomerAttr() : bool
     {
-        return 'ads';
+        $customerData = $this->_backendSession->getCustomerData();
+        $customerAttribute = $customerData['account'];
+        if ($customerAttribute !== null) {
+            $value = (bool)$customerAttribute[AddCustomerAttributeCreditHold::ATTRIBUTE_CODE];
+        } else {
+            $value = 0;
+        }
+
+        return $value;
     }
 
-    public function getMessage()
+    /**
+     * Get current customer id.
+     *
+     * @return int
+     */
+    public function getCustomerId() : int
+    {
+        $customerData = $this->_backendSession->getCustomerData();
+        $customerAttribute = $customerData['account'];
+        if ($customerAttribute !== null) {
+            $value = $customerAttribute['id'];
+        } else {
+            $value = 0;
+        }
+
+        return $value;
+    }
+
+    /**
+     * Set customer id to array in admin session.
+     */
+    public function setCustomerIdToAdminSession() : void
+    {
+        $customersId = $this->getCustomersId();
+        $customersId[] = $this->getCustomerId();
+        $this->authSession->setData('customers_id', $customersId);
+    }
+
+    /**
+     * Check for once show
+     *
+     * @return bool
+     */
+    public function checkId() : bool
+    {
+        $currentCustomerId = $this->getCustomerId();
+        $arrayCustomersId = $this->getCustomersId();
+
+        if (!$arrayCustomersId) {
+            $check = false;
+        } else {
+            $check = in_array($currentCustomerId, $arrayCustomersId);
+        }
+
+        return $check;
+    }
+
+    /**
+     * Get customers id from admin session.
+     */
+    public function getCustomersId()
+    {
+        if ($this->authSession->getData('customers_id')){
+            $customers = $this->authSession->getData('customers_id');
+        } else {
+            $customers = [];
+        }
+
+        return $customers;
+    }
+
+    /**
+     * Get message.
+     *
+     * @return string
+     */
+    public function getMessage() : string
     {
         return $this->credithold->getMessage();
     }
