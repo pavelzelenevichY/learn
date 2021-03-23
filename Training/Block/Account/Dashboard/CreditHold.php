677cdbdb27670e71893e6807ba33bb229fc9b633
pavelzelenevichY <pzelenevich@codifi.me>
2021-03-23 12:19:12 +0300

    [MMT-7] - Remove logic from block. Remove setFlag from block and template.

--- Training/Block/Account/Dashboard/CreditHold.php
+++ Training/Block/Account/Dashboard/CreditHold.php
@@ -10,12 +10,10 @@ declare(strict_types=1);
 
 namespace Codifi\Training\Block\Account\Dashboard;
 
-use Magento\Cms\Api\BlockRepositoryInterface;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
-use Magento\Customer\Model\Session;
-use Codifi\Training\Setup\Patch\Data\AddCustomerAttributeCreditHold;
-use Magento\Framework\Escaper;
+use Codifi\Training\Model\ConfigProvider;
+use Codifi\Training\Model\CustomerSession;
 
 /**
  * Class CreditHold
@@ -24,107 +22,45 @@ use Magento\Framework\Escaper;
 class CreditHold extends Template
 {
     /**
-     * Path to the system option Enabled - Credit Hold.
+     * Customer session
      *
-     * @var string
+     * @var CustomerSession
      */
-    const PATH_OPTION_ENABLE = 'codifi/credit_hold/active';
+    private $customerSession;
 
     /**
-     * Path to the system option Value - Message Credit Hold.
+     * Config Provider
      *
-     * @var string
+     * @var ConfigProvider
      */
-    const PATH_OPTION_MESSAGE = 'codifi/credit_hold/message';
+    private $configProvider;
 
     /**
-     * Name of session attribute for once show popup.
-     *
-     * @var string
-     */
-    const SESSION_FLAG = 'flag';
-
-    /**
-     * Customer session.
-     *
-     * @var Session
-     */
-    private $session;
-
-    /**
-     * Block Repository Interface
-     *
-     * @var BlockRepositoryInterface
-     */
-    private $blockRepository;
-
-    /**
-     * @var Escaper
-     */
-    public $escaper;
-
-    /**
-     * CreditHold constructor
+     * CreditHold constructor.
      *
      * @param Context $context
-     * @param Session $session
-     * @param BlockRepositoryInterface $blockRepository,
      * @param array $data
      */
     public function __construct(
         Context $context,
-        Session $session,
-        BlockRepositoryInterface $blockRepository,
+        CustomerSession $customerSession,
+        ConfigProvider $configProvider,
         array $data = []
     ) {
-        $this->session = $session;
-        $this->blockRepository = $blockRepository;
+        $this->customerSession = $customerSession;
+        $this->configProvider = $configProvider;
+
         parent::__construct($context, $data);
     }
 
     /**
-     * Get credit_hold attribute value
+     * Check function
      *
      * @return bool
      */
-    public function getCustomerAttr() : bool
+    public function check() : bool
     {
-        $customerData = $this->session->getCustomerData();
-        $customerAttribute = $customerData->getCustomAttribute(AddCustomerAttributeCreditHold::ATTRIBUTE_CODE);
-        if ($customerAttribute !== null) {
-            $value = (bool)$customerAttribute->getValue();
-        } else {
-            $value = 0;
-        }
-        return $value;
-    }
-
-    /**
-     * Get options enabled and message value and check customer attribute.
-     *
-     * @return bool
-     */
-    public function isCreditHoldConfigEnabled() : bool
-    {
-        return (bool)$this->_scopeConfig->getValue(self::PATH_OPTION_ENABLE, $this->_scopeConfig::SCOPE_TYPE_DEFAULT);
-    }
-
-    /**
-     * Flag for onse show popup
-     *
-     * @return bool
-     */
-    public function getFlag() : bool
-    {
-        return (bool)$this->session->getData(self::SESSION_FLAG);
-    }
-
-    /**
-     * Set flag value true
-     */
-    public function setFlag() : void
-    {
-        $this->session->setData(self::SESSION_FLAG, true);
+        return $this->customerSession->check();
     }
 
     /**
@@ -134,7 +70,6 @@ class CreditHold extends Template
      */
     public function getMessage() : string
     {
-        $message = $this->_scopeConfig->getValue(self::PATH_OPTION_MESSAGE);
-        return $this->escapeHtml($message);
+        return $this->customerSession->getMessageAndCallSetFlag();
     }
 }
