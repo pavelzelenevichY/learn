0d5d27df066d76b48ce7c2dceff6979e14764873
pavelzelenevichY <pzelenevich@codifi.me>
2021-03-23 19:24:58 +0300

    [MMT-1] - Added routes.xml and Save.php controller.

---
+++ Training/Controller/Note/Save.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace Codifi\Training\Controller\Note;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\ResponseInterface;
+use Codifi\Training\Model\CustomerNote;
+use Codifi\Training\Model\ResourceModel\CustomerNote as CustomerNoteResource;
+
+class Save extends Action
+{
+    /**
+     * Customer note model.
+     *
+     * @var CustomerNote
+     */
+    private $customerNote;
+
+    /**
+     * Customer note resource model.
+     *
+     * @var CustomerNoteResource
+     */
+    private $customerNoteResource;
+
+    /**
+     * Save constructor.
+     *
+     * @param Context $context
+     * @param CustomerNote $customerNote
+     * @param CustomerNoteResource $customerNoteResource
+     */
+    public function __construct(
+        Context $context,
+        CustomerNote $customerNote,
+        CustomerNoteResource $customerNoteResource
+    ) {
+        parent::__construct($context);
+        $this->customerNote = $customerNote;
+        $this->customerNoteResource = $customerNoteResource;
+    }
+
+    /**
+     * Execute
+     *
+     * @return ResponseInterface|\Magento\Framework\Controller\Result\Redirect|\Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $data = $this->getRequest()->getParams();
+
+        $customerNoteModel = $this->customerNote;
+        $customerNoteModel->setData($data);
+
+        try {
+            $this->customerNoteResource->save($customerNoteModel);
+            $this->messageManager->addSuccessMessage('');
+        } catch (\Exception $exception) {
+            $this->messageManager->addErrorMessage(__('Note text is missed.'));
+        }
+        $redirect = $this->resultRedirectFactory->create();
+        $redirect->setPath('/');
+
+        return $redirect;
+    }
+}
